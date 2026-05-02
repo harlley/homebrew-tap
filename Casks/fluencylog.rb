28454ee8c812499f4d39e59d2ff09dbcc7bf8156
Harlley Oliveira <harlley@gmail.com>
2026-05-02 16:40:40 +0100

    fluencylog 0.1.3: fix empty sha256 (pipeline bug)

--- Casks/fluencylog.rb
+++ Casks/fluencylog.rb
@@ -1,6 +1,6 @@
 cask "fluencylog" do
   version "0.1.3"
-  sha256 ""
+  sha256 "ed44c787d09f4f310694eb237102b0d479814f37f6dd7e44d4347971d30d0601"
 
   url "https://github.com/harlley/fluencylog/releases/download/v#{version}/FluencyLog-#{version}-macos.zip"
   name "FluencyLog"
